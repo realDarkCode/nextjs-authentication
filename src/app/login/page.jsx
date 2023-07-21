6cc385986cc826a1c43063c03e0af2e2aed8982a
Tanzil Islam <tanzil.ncpsc@gmail.com>
2023-07-21 12:30:23 +0600

    initial commit

---
+++ src/app/login/page.jsx
@@ -0,0 +1,5 @@
+const LoginPage = () => {
+  return <div className="text-center text-xl">LoginPage</div>;
+};
+
+export default LoginPage;
---
+++ src/app/signup/page.jsx
@@ -0,0 +1,11 @@
+const SignUpPage = () => {
+  return (
+    <>
+      <div>
+        <h2>Signup page</h2>
+      </div>
+    </>
+  );
+};
+
+export default SignUpPage;
