8c96148b91f040ef5902d82da1cca1caf27e1ce5
Tanzil Islam <tanzil.ncpsc@gmail.com>
2023-07-21 16:30:33 +0600

    feat: implement signup and login page design

---
+++ src/core/DBConnection.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+export async function connect() {
+  try {
+    await mongoose.connect(process.env.DATABASE_URI);
+    const connection = mongoose.connection;
+
+    connection.on("connected", () => {
+      console.log(`Connected successfully to database ${connection.name}`);
+    });
+    connection.on("error", (error) => {
+      console.log("error while connecting to database");
+      console.log(error);
+      process.exit(1);
+    });
+  } catch (error) {
+    console.log("Something went wrong with database");
+    console.log(error);
+  }
+}
---
+++ src/helpers/validation.js
@@ -0,0 +1,25 @@
+const validationError = (msg) => {
+  let err = new Error(msg);
+  err.name = "[Validation Error]";
+  return err;
+};
+
+const defaultOptions = { dataType: "string", minLen: 8, maxLen: 30 };
+const validate = (data = "", fieldName = "data", options) => {
+  options = { ...defaultOptions, ...options };
+  const { dataType, minLen, maxLen } = options;
+
+  if (!data) throw validationError(`${fieldName} is required`);
+  if (typeof data !== dataType)
+    throw validationError(`${fieldName} should be ${dataType} type`);
+  if (data.toString().length < minLen)
+    throw validationError(
+      `${fieldName} length must be at least ${minLen} characters long`
+    );
+  if (data.toString().length > maxLen)
+    throw validationError(
+      `${fieldName} length must be within ${minLen} characters`
+    );
+};
+
+export default validate;
